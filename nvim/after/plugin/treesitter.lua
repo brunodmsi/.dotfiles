86063eb1dcc7131a8cf325f8c1ec27d04bc59431
Bruno De Masi <bruno@demasi.dev>
2023-07-06 09:19:41 -0300

    nvim: remove help parser from treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup { 
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "help", "javascript", "typescript", "php", "c", "lua", "vim", "css", "scss" },
+  ensure_installed = { "javascript", "typescript", "php", "c", "lua", "vim", "css", "scss" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
